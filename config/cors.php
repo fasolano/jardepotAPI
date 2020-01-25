4374b90dbe2fd09f4cb625eb9edd1fd7ee82c589
solano <fasolanof@gmail.com>
2020-01-25 14:41:41 -0600

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,8 @@
 <?php
-
+header('content-type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 return [
 
     /*
