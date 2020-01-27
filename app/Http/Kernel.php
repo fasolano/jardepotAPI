211973f140361a368bbc8cea0cadf9e8e331dbec
Fernando Jardepot <fasolanof@gmail.com>
2020-01-27 13:45:43 -0600

    cambios mercado pago y kernel (numero de peticiones)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Barryvdh\Cors\HandleCors::class,
-            'throttle:120,1',
+            'throttle:240,1',
             'bindings',
         ],
     ];
