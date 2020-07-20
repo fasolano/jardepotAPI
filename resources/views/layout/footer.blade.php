200fe7abbe6060c10ac1be75ec9242087faa8466
solano <fasolanof@gmail.com>
2020-07-20 11:01:24 -0500

    se agregan los iconos de material

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,10 +2,21 @@
     <div class="row justify-content-md-center">
         <div class="col-md-9 my-4">
             <div class="row">
-                <div class="col-md-8">
-                    <p style="font-size: 1.17em; font-weight: 600;">INFORMACIÓN DE CONTACTO</p>
+                <div class="col-md-7">
+                    <p class="font-weight-bold">INFORMACIÓN DE CONTACTO</p>
+                    <div>
+                        <p><i class="material-icons">location_on</i> Av. Emiliano Zapata No. 129, Tlaltenango 62170, Cuernavaca Morelos.</p>
+                        <p><i class="material-icons">location_on</i> Lunes - Viernes / 9:00AM - 6:00PM</p>
+                        <p><i class="material-icons">location_on</i> Sabado / 9:00AM - 2:00PM</p>
+                    </div>
+                    <p class="font-weight-bold">Atención a Comercializadoras y Mayoristas:</p>
+                    <div>
+                        <p><i class="material-icons">location_on</i> (55) 4459 8506.</p>
+                        <p><i class="material-icons">location_on</i> Lunes - Viernes / 9:00AM - 6:00PM</p>
+                        <p><i class="material-icons">location_on</i> Sabado / 9:00AM - 2:00PM</p>
+                    </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <img src="{{asset('assets/images/otros/maps.png')}}" alt="Google Maps">
                 </div>
             </div>
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -11,6 +11,7 @@
 <Meta name="robots" content="index,follow">
 
 <link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap-grid.min.css')}}">
