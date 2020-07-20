2e1fab97076dc326652688900e5b0216f721ad35
emlyn <sistemas2@jardepot.com>
2020-07-20 10:56:16 -0500

    cambios head general ccc

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -12,8 +12,9 @@
 
 <link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i' rel='stylesheet' type='text/css'>
 
-<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap-grid.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap-reboot.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/general.css')}}">
 
 
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,12 +1,18 @@
 {{--
-    <div>Llámanos al : (800) 212 9225</div>
-    <div>CDMX (55) 4996 8849</div>
-    <div>GDL (33) 1728 3353</div>
-    <div>EDOMX (722) 648 1040</div>
-    <div>Más teléfonos</div>--}}
+    --}}
 
-<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Jardepot</a>
+<nav class="navbar sticky-top navbar-expand-xl navbar-dark bg-dark navbar-heigth">
+    <div class="align-cont">
+    <div class="div-navbar div-navbar-position">
+        <div>Llámanos al : (800) 212 9225</div>
+        <div>CDMX (55) 4996 8849</div>
+        <div>GDL (33) 1728 3353</div>
+        <div>EDOMX (722) 648 1040</div>
+        <div>Más teléfonos ></div>
+    </div>
+    </div>
+    <br>
+    <a class="navbar-brand" href="#"><img class="logo-navbar" src="{{asset('img/logos/logoJardepot.png')}}"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,45 +20,55 @@
     <div class="collapse navbar-collapse" id="navbarsExample06">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+{{--                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>--}}
             </li>
         </ul>
         <form class="form-inline my-2 my-md-0">
-            <input class="form-control" type="text" placeholder="Search">
+            <input class="form-control" type="text"  placeholder="Search">
         </form>
     </div>
 </nav>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-xl">
-        <a class="navbar-brand" href="#">Container XL</a>
+{{--        <a class="navbar-brand" href="#">Container XL</a>--}}
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarsExample07XL">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
+{{--                <li class="nav-item active">--}}
+{{--                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>--}}
+{{--                </li>--}}
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Agricultura</a>
                     <div class="dropdown-menu" aria-labelledby="dropdown07XL">
                         <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Sanitizacion</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown07XL">
+                        <a class="dropdown-item" href="#">Action</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Jardinería</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown07XL">
+                        <a class="dropdown-item" href="#">Action</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">construccion</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown07XL">
+                        <a class="dropdown-item" href="#">Action</a>
                     </div>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-md-0">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-            </form>
+            <a class="mr-2 nav-link btn btn-danger" href="#">Ofertas</a>
+            <a class="mr-2 nav-link btn btn-success" href="#">Whatsapp</a>
+            <a class="mr-2 nav-link btn btn-warning" href="#">Refacciones</a>
         </div>
     </div>
 </nav>
