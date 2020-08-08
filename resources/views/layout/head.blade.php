81d817c09694f714698fa2e8128e2f65fdcacd55
emlyn <sistemas2@jardepot.com>
2020-08-08 02:01:50 -0500

    se minifican js y css

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -13,11 +13,11 @@
 <link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i' rel='stylesheet' type='text/css'>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap-grid.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap-reboot.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/general.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/layouts/footer.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/layouts/navbar.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/general.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/layouts/footer.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/layouts/navbar.min.css')}}">
 <script type="text/javascript" src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
 
