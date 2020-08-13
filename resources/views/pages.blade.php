5452ec1c4873786937debe89b135fa440fb4123c
emlyn <sistemas2@jardepot.com>
2020-08-13 12:22:54 -0500

    cambios estruct

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -2,13 +2,16 @@
 <html lang="es">
 <head>
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T2GB24V');</script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T2GB24V');</script>
     <!-- End Google Tag Manager -->
     {{--  Metatitle, metadescripcion, titulo de página, etc.  --}}
     @yield('metaData')
 <!-- Facebook Pixel Code -->
-{{--    <script>
+    <script>
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -22,7 +25,7 @@
     </script>
     <noscript>
         <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=209203906543019&ev=PageView&noscript=1"/>
-    </noscript>--}}
+    </noscript>
     <!-- End Facebook Pixel Code -->
     @include('layout.head')
 
@@ -32,7 +35,9 @@
 
 <body style="font-family: Roboto !important;">
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2GB24V" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2GB24V" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
