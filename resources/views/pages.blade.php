d3f6e5a131e6ace8fe566b83b12416753d4df270
emlyn <sistemas2@jardepot.com>
2020-08-13 10:52:59 -0500

    se descomentan lso tag manager

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -2,8 +2,8 @@
 <html lang="es">
 <head>
     <!-- Google Tag Manager -->
-{{--    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T2GB24V');</script>--}}
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T2GB24V');</script>
     <!-- End Google Tag Manager -->
     {{--  Metatitle, metadescripcion, titulo de página, etc.  --}}
     @yield('metaData')
@@ -33,7 +33,7 @@
 
 <body style="font-family: Roboto !important;">
 <!-- Google Tag Manager (noscript) -->
-{{--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2GB24V" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>--}}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2GB24V" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
