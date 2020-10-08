d533da2c72b3e47fb5e918cf347c278acd1508e3
emlyn <sistemas2@jardepot.com>
2020-10-08 16:59:23 -0500

    se bloquea coiar y click derecho

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -7,6 +7,11 @@ $.ajaxSetup({
 // const ruta = '/';
 
 $(document).ready(function (){
+    //Disable cut copy paste
+    $('body').bind('cut copy paste', function (e) { e.preventDefault(); });
+    //Disable mouse right click
+    $("body").on("contextmenu",function(e){return false;});
+
     $('[data-toggle="tooltip"]').tooltip();
     getCartProducts();
     $(window).on('scroll', function ($event) {
