afe7a87658118fc8f650ae60666dc631fd09d568
emlyn <sistemas2@jardepot.com>
2020-10-19 10:16:19 -0500

    se cambian numero whastapp erroneo y se añden alts y titles a iamgenes faltantes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -67,7 +67,9 @@
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <a href="{{url('ofertas')}}">
-                                <img class="img-fluid mx-auto d-block" src="{{asset('assets/images/banner/banner1.jpg')}}" style="max-width: 95%">
+                                <img class="img-fluid mx-auto d-block" src="{{asset('assets/images/banner/banner1.jpg')}}"
+                                     alt="Oferta Jardepot" title="oferta Jardepot"
+                                     style="max-width: 95%">
                             </a>
                         </div>
                     </div>
