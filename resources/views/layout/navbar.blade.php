ac6c39295411a77b6f4f9f483dc7f98760c8db8e
emlyn <sistemas2@jardepot.com>
2020-11-25 16:00:35 -0600

    se modifica la barra

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -215,17 +215,17 @@
                 <picture>
                     <source srcset="{{asset('img/logos/logoJardepot.webp')}}" type="image/webp">
                     <source srcset="{{asset('img/logos/logoJardepot.png')}}" type="image/png">
-                    <img class="logo-navbar" style="width: 80%" src="{{asset('img/logos/logoJardepot.png')}}"
+                    <img class="logo-navbar" style="width: 60%" src="{{asset('img/logos/logoJardepot.png')}}"
                          alt="Logo Jardepot" title="Logo Jardepot">
                 </picture>
                 {{--<img class="logo-navbar" style="width: 40px" src="{{asset('assets/images/otros/calaberita.png')}}"
                      alt="Imagen Temporada title="Imagen Temporada">--}}
             </a>
         </div>
-        <div class="col-4" style="margin-top: 8px">
+        <div class="col-4" style="margin-top: 1px">
             <button class="btn dropdown-toggle" style="color: #FFF" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span style="color: #fff !important;cursor:pointer;">
-                    <i class="material-icons" style="color: #f68600;font-size: 30px !important;">shopping_cart</i>
+                    <i class="material-icons" style="color: #f68600;font-size: 28px !important;">shopping_cart</i>
                     <span class="cart-items-count" id="items-count-nav2"></span>
                 </span>
             </button>
@@ -280,9 +280,22 @@
             </div>
         </form>
     </div>
+    <div class="row">
+        <div class="col-12">
+            <a class="mr-2 btn-success btn-sm d-flex" target="_blank" href="https://wa.me/525551857805?text=Hola,%20me%20gustaría%20saber%20" style="text-decoration: none;"  rel="noopener">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
+                     focusable="false" width="20px" height="20px" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+                     preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01A9.816 9.816 0 0 0 12.04 2m.01 1.67c2.2 0 4.26.86 5.82 2.42a8.225 8.225 0 0 1 2.41 5.83c0 4.54-3.7 8.23-8.24 8.23c-1.48 0-2.93-.39-4.19-1.15l-.3-.17l-3.12.82l.83-3.04l-.2-.32a8.188 8.188 0 0 1-1.26-4.38c.01-4.54 3.7-8.24 8.25-8.24M8.53 7.33c-.16 0-.43.06-.66.31c-.22.25-.87.86-.87 2.07c0 1.22.89 2.39 1 2.56c.14.17 1.76 2.67 4.25 3.73c.59.27 1.05.42 1.41.53c.59.19 1.13.16 1.56.1c.48-.07 1.46-.6 1.67-1.18c.21-.58.21-1.07.15-1.18c-.07-.1-.23-.16-.48-.27c-.25-.14-1.47-.74-1.69-.82c-.23-.08-.37-.12-.56.12c-.16.25-.64.81-.78.97c-.15.17-.29.19-.53.07c-.26-.13-1.06-.39-2-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.12-.24-.01-.39.11-.5c.11-.11.27-.29.37-.44c.13-.14.17-.25.25-.41c.08-.17.04-.31-.02-.43c-.06-.11-.56-1.35-.77-1.84c-.2-.48-.4-.42-.56-.43c-.14 0-.3-.01-.47-.01z" fill="#fff"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" />
+                </svg>Whatsapp</a>
+        </div>
+        <div class="col-12 mt-2">
+            <a class="mr-2 btn-warning btn-sm d-flex" href="tel:8002129225"
+               style="text-decoration: none;"><i class="material-icons iconMod">call</i>Llámanos</a>
+        </div>
+    </div>
 </nav>
 
-<nav class="navbar navbar-light bg-light  d-block d-sm-block d-lg-none">
+{{--<nav class="navbar navbar-light bg-light  d-block d-sm-block d-lg-none">
     <div class="row">
         <div class="col-6">
             <a class="mr-2 btn-success btn-sm d-flex" target="_blank" href="https://wa.me/525551857805?text=Hola,%20me%20gustaría%20saber%20" style="text-decoration: none;"  rel="noopener">
@@ -296,7 +309,7 @@
                style="text-decoration: none;"><i class="material-icons iconMod">call</i>Llámanos</a>
         </div>
     </div>
-</nav>
+</nav>--}}
 
 <!-- Modal Teléfonos-->
 <div class="modal fade" id="modalTelefonos" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
@@ -304,7 +317,7 @@
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalTelefonosLabel">Nuestros números teléfonicos</h5>
+                <h5 class="modal-title" id="modalTelefonosLabel">LLámanos</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -383,7 +396,24 @@
                 </div>
                 <div class="modal-body">
                     <div class="container">
-                        <a class="telModal" href="tel:5544598506"><i class="material-icons">call</i> 55 4459 8506</a>
+                        <div style="padding: 0;" class="row">
+                            <div class="col-md-12">
+                                <a class="telModal" href="tel:5544598506"><i class="material-icons">call</i> 55 4459 8506</a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div style="padding: 0;" class="row">
+                            <div class="col-md-1"><i class="material-icons">email</i></div>
+                            <div class="col-md-4">
+                                 <a href="mailto:mayoreo@jardepot.com" class="telModal">mayoreo@jardepot.com</a>
+                            </div>
+                            <div class="col-md-3">
+                                <a href="mailto:ventas@jardepot.com" class="telModal">ventas@jardepot.com</a>
+                            </div>
+                            <div class="col-md-4">
+                                <a href="mailto:refacciones@jardepot.com" class="telModal">refacciones@jardepot.com</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
