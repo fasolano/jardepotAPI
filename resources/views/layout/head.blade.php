320cfd1fa04c41d100bab46f4b3790af30adbcd8
emlyn <sistemas2@jardepot.com>
2020-12-11 09:32:33 -0600

    metas cache

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -10,6 +10,10 @@
 <meta name="googlebot" content="index,follow" />
 <Meta name="robots" content="index,follow">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+<meta http-equiv="Expires" content="0" />
+<meta http-equiv="Pragma" content="no-cache" />
+
 <link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='stylesheet' type='text/css'>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
