c0b98f2446f95e9c194672bb511411ac931c8f87
solano <fasolanof@gmail.com>
2021-01-14 10:18:43 -0600

    Se remueve el hotjar

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -91,17 +91,6 @@
     <noscript>
         <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=209203906543019&ev=PageView&noscript=1"/>
     </noscript>
-    <!-- Hotjar Tracking Code for www.jardepot.com -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2182129,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
     <!-- End Facebook Pixel Code -->
     @include('layout.head')
 
