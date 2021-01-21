68dde482907ea320701f94b75e1aabc56b316f91
emlyn <sistemas2@jardepot.com>
2021-01-21 11:29:00 -0600

    error title vista productos

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -7,8 +7,8 @@
     <meta name="description" content="{{isset($descriptionLevel2->metadescription)?$descriptionLevel2->metadescription:""}}">
 {{--    <meta name="keywords" content="{{isset($descriptionLevel2->keywords)?$descriptionLevel2->keywords:""}}">--}}
 
-    <meta property="og:title" content="{{ $descriptionLevel2->metatitle}}" />
-    <meta property="og:description" content="{{ $descriptionLevel2->metadescription}}" />
+    <meta property="og:title" content="{{ isset($descriptionLevel2->metatitle)?substr($descriptionLevel2->metatitle, 0,70):""}}" />
+    <meta property="og:description" content="{{ isset($descriptionLevel2->metatitle)?substr($descriptionLevel2->metatitle, 0,70):""}}" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{isset($canonical)?$canonical:'https://www.jardepot.com/'}}" />
     <meta property="og:image" content="{{asset('img/logos/logoOG.jpg')}}" />
