b2acb4ccf7314deb632f617116419a39b37fb1f6
emlyn <sistemas2@jardepot.com>
2021-01-22 09:55:14 -0600

    se arregla tamaños de las listas de navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -153,8 +153,8 @@
 {{--                    <ul class="navbar-nav mr-auto col-md-8 d-flex flex-wrap justify-content-center align-items-center" id="menu">--}}
                     <ul class="navbar-nav mr-auto col-md-9 d-flex flex-wrap justify-content-center align-items-center" id="menu">
                         @foreach ($navbar as $key => $categoria1)
-                            <li class="nav-item dropdown mx-2"><a class="d-flex flex-wrap justify-content-center align-items-center" title="{{$categoria1['nivel1']}}"  href="javascript:;">{{$categoria1['nivel1']}} <i class="material-icons iconMod">keyboard_arrow_down</i></a>
-                                <ul class="dropdown-menu">
+                            <li class="nav-item dropdown mx-2 column-items" id="prueba1"><a class="d-flex flex-wrap justify-content-center align-items-center" title="{{$categoria1['nivel1']}}"  href="javascript:;">{{$categoria1['nivel1']}} <i class="material-icons iconMod">keyboard_arrow_down</i></a>
+                                <ul class="{{count($categoria1['nivel2']) > 10 ? 'column-navbar':''}} dropdown-menu">
                                     @foreach($categoria1['nivel2'] as $categoria2)
                                     <li class="dropdown-item"><a class="text-left text-muted" title="{{$categoria2['name']}}" href="{{url($categoria2['href'])}}">{{$categoria2['name']}}</a></li>
                                     @endforeach
