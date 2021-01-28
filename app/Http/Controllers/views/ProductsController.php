30368e6b766d20ba9567b4ea3f2f0eb72d4b6c5f
emlyn <sistemas2@jardepot.com>
2021-01-28 15:47:13 -0600

    se cambian revolvedoras

--- app/Http/Controllers/views/ProductsController.php
+++ app/Http/Controllers/views/ProductsController.php
@@ -115,6 +115,8 @@ class ProductsController extends Controller {
         $urlsArray['sanitizacion-y-fumigacion/parihuelas']='agricultura/parihuelas';
         $urlsArray['sanitizacion-y-fumigacion/desinfectantes']='sanitizacion/desinfectantes';
         $urlsArray['sanitizacion-y-fumigacion/termonebulizadoras']='sanitizacion/termonebulizadoras';
+        $urlsArray['construccion/revolvedoras']='construccion/revolvedoras-de-concreto';
+
         if(array_key_exists($url,$urlsArray)){
             return $urlsArray[$url];
         }else{
