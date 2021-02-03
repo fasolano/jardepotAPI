a009d13906a05ba47243eaf1b0e2184c174b9d0b
emlyn <sistemas2@jardepot.com>
2021-02-03 17:06:07 -0600

    se cambia alineacion navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -159,9 +159,9 @@
                     </ul>--}}
 
 {{--                    <ul class="navbar-nav mr-auto col-md-8 d-flex flex-wrap justify-content-center align-items-center" id="menu">--}}
-                    <ul class="navbar-nav mr-auto col-md-12 d-flex flex-wrap justify-content-center align-items-center" id="menu">
+                    <ul class="navbar-nav mr-auto col-md-12 d-flex flex-wrap justify-content-left align-items-center" id="menu">
                         @foreach ($navbar as $key => $categoria1)
-                            <li class="nav-item dropdown mx-2 column-items" id="prueba1"><a class="d-flex flex-wrap justify-content-center align-items-center" title="{{$categoria1['nivel1']}}"  href="javascript:;">{{$categoria1['nivel1']}} <i class="material-icons iconMod">keyboard_arrow_down</i></a>
+                            <li class="nav-item dropdown mx-1 column-items" id="prueba1"><a class="d-flex flex-wrap justify-content-center align-items-center" title="{{$categoria1['nivel1']}}"  href="javascript:;">{{$categoria1['nivel1']}} <i class="material-icons iconMod">keyboard_arrow_down</i></a>
                                 @if(isset($categoria1['nivel2']))
                                 <ul class="{{count($categoria1['nivel2']) > 10 ? 'column-navbar':''}} dropdown-menu">
 
