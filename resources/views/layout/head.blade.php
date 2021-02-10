b67bbe7b253b9d8f433dee9446299ff5656899fe
emlyn <sistemas2@jardepot.com>
2021-02-10 12:59:09 -0600

    cambio posicion

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -12,8 +12,8 @@
 <meta http-equiv="Expires" content="0" />
 <meta http-equiv="Pragma" content="no-cache" />
 
-<link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='stylesheet' type='text/css'>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='stylesheet' type='text/css'>
 
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/layouts/layoutsV1.1.min.css')}}">
