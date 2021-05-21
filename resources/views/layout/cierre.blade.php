7fab9839f782976335eafb6920902330ff97b2ab
Zuriel Barrios <street_dark_fighter@hotmail.com>
2021-05-21 17:19:08 -0500

    asdasdasd

--- resources/views/layout/cierre.blade.php
+++ resources/views/layout/cierre.blade.php
@@ -4,4 +4,4 @@
 <script async src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 <script async src="{{asset('assets/js/popper.min.js')}}"></script>
 {{--<script type="text/javascript" src="{{asset('assets/js/jquery.validate.min.js')}}"></script>--}}
-<script async src="{{asset('assets/js/generalV1.min.js')}}"></script>
+<script src="{{asset('assets/js/generalV1.min.js')}}"></script>
