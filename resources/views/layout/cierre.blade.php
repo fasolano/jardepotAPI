46605e5d7c2c7bdf9d5314d9c5f6d378b3771afa
Zuriel Barrios <street_dark_fighter@hotmail.com>
2021-05-21 16:54:58 -0500

    async scripts

--- resources/views/layout/cierre.blade.php
+++ resources/views/layout/cierre.blade.php
@@ -1,7 +1,7 @@
-<script type="text/javascript" src="{{ asset('assets/js/js.cookie.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/path.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('assets/js/js.cookie.min.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('assets/js/path.js') }}"></script>
 
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<script async src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+<script async src="{{asset('assets/js/popper.min.js')}}"></script>
 {{--<script type="text/javascript" src="{{asset('assets/js/jquery.validate.min.js')}}"></script>--}}
-<script src="{{asset('assets/js/generalV1.min.js')}}"></script>
+<script async src="{{asset('assets/js/generalV1.min.js')}}"></script>
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -12,11 +12,11 @@
 <meta http-equiv="Expires" content="0" />
 <meta http-equiv="Pragma" content="no-cache" />
 
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" onload="this.media='all'">
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='stylesheet' type='text/css' onload="this.media='all'">
 
-<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/layouts/layoutsV1.1.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/general.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" onload="this.media='all'">
+<link rel="stylesheet" href="{{asset('assets/css/layouts/layoutsV1.1.min.css')}}" onload="this.media='all'">
+<link rel="stylesheet" href="{{asset('assets/css/general.min.css')}}" onload="this.media='all'">
 <script type="text/javascript"  src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
 
