baf703e45052bcc9454eb4b929c7a02264ec2382
Zuriel Barrios <street_dark_fighter@hotmail.com>
2021-05-21 17:41:09 -0500

    rollback styles

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -12,11 +12,11 @@
 <meta http-equiv="Expires" content="0" />
 <meta http-equiv="Pragma" content="no-cache" />
 
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="preload" as="style" onload="this.rel='stylesheet'">
-<link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='preload' as="style" type='text/css' onload="this.rel='stylesheet'">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap' rel='stylesheet' type='text/css'>
 
-<link rel="preload" as="style" href="{{asset('assets/css/bootstrap.min.css')}}" onload="this.rel='stylesheet'">
-<link rel="preload" as="style" href="{{asset('assets/css/layouts/layoutsV1.1.min.css')}}" onload="this.rel='stylesheet'">
-<link rel="preload" as="style" href="{{asset('assets/css/general.min.css')}}" onload="this.rel='stylesheet'">
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/layouts/layoutsV1.1.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/general.min.css')}}">
 <script type="text/javascript"  src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
 
