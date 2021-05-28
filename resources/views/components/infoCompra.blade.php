777049ca616425eed679b9df3fb65025bccc5e80
Zuriel Barrios <street_dark_fighter@hotmail.com>
2021-05-28 09:18:38 -0500

    modified home

--- resources/views/components/infoCompra.blade.php
+++ resources/views/components/infoCompra.blade.php
@@ -1,91 +1,4 @@
-<div class="row">
-    <div class="col-12" style="background-color: #3d3c3b; color: white;
-        border-bottom: 4px solid #e76a27;">
-        <h2 class="text-center my-3">¿Quiénes somos?</h2>
-        <p class="text-center" style="font-size: 18px;">
-            Somos su tienda líder en México en maquinaria ligera para agricultura,
-            construcción, jardinería o sanitización tanto para profesionales
-            como para uso particular. Contamos con equipamientos y modelos de las
-            mejores marcas del mercado que garantizan una gran eficiencia y vida útil.
-        </p>
-    </div>
-</div>
-
-<div class="row pb-4 shadow" style="background-color: #ffffff; box-shadow:rgba(0, 0, 0, 0.54);">
-    <div class="col-md-4 mt-4 col-lg-4 col-sm-12">
-        <div class="row">
-            <div class="col-md-10 offset-md-1 text-center w-90">
-                <img
-                    class="justify-self-center"
-                    src="/assets/images/icons/equipos-jardineria.png"
-                    title="Equipos para jardinería"
-                    height="60"
-                    width="60"/>
-                <div class="mt-4">
-                    <div class="mb-n2"><strong>
-                        Equipos de Jardinería Profesional
-                    </strong></div>
-                    <br/>
-                    <p style="font-size: 12px;" class="mt-n1">
-                        Ponemos a su disposición los equipos de jardinería
-                        más modernos para la <strong>poda de árboles, cortar el césped
-                        o malas hierbas o sopladoras</strong> para realizar tareas de limpieza.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4 mt-4 col-lg-4 col-sm-12">
-        <div class="row">
-            <div class="col-10 offset-md-1 text-center">
-                <img
-                    class="justify-self-center"
-                    src="/assets/images/icons/equipos-agricolas.png"
-                    title="Equipos para agricultura"
-                    height="60"
-                    width="60"/>
-                <div class="mt-4">
-                    <div class="mb-n2"><strong>
-                        Equipos Agrícolas
-                    </strong></div>
-                    <br>
-                    <p style="font-size: 12px;" class="mt-n1">
-                        Todo el equipamiento en maquinaria para <strong>mejorar el
-                        rendimiento de lo cultivos.</strong> Encuentre en nuestro extenso
-                        catálogo, herramientas de primeras marcas para labrar tierras,
-                        separar los granos en las cocechas.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4 mt-4 col-lg-4 col-sm-12">
-        <div class="row">
-            <div class="col-10 offset-md-1 text-center">
-                <img
-                    class="justify-self-center"
-                    src="/assets/images/icons/equipos-construccion.png"
-                    title="Equipos para construcción"
-                    height="60"
-                    width="60"/>
-                <div class="mt-4">
-                    <div class="mb-n2"><strong>
-                        Maquinaria Ligera para Construcción
-                    </strong></div>
-                    <br>
-                    <p style="font-size: 12px;" class="mt-n1">
-                        Contamos con el equipamiento necesario para la ejecución
-                        de una obra, para <strong>compactar superfucies, cortadoras de piso,
-                        equipos para romper o demolición.</strong>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-{{-- <div class="row mt-2" style="background-color: #F2F2F2">
+<div class="row mt-2" style="background-color: #F2F2F2">
     <div class="col-md-3">
         <div class="row">
             <div class="col-md-3 justify-content-center align-self-center">
@@ -131,4 +44,4 @@
             </div>
         </div>
     </div>
-</div> --}}
+</div>
